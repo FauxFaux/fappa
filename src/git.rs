c36f90730cfcfd6b9280a5385ef2962780767afd
Chris West (Faux) <git@goeswhere.com>
2018-05-25 14:51:18 +0100

    fetch missing repos into the cache

--- src/git.rs
+++ src/git.rs
@@ -1,12 +1,38 @@
+use std::fs;
 use std::path::PathBuf;
 
 use failure::Error;
 use git2;
 use url::Url;
 
-pub fn check_cloned<S: AsRef<str>>(url: S) -> Result<PathBuf, Error> {
+#[derive(Copy, Clone, Debug)]
+enum GitSpecifier {
+    Hash(git2::Oid),
+}
+
+pub struct LocalRepo {
+    specifier: GitSpecifier,
+    path: PathBuf,
+}
+
+impl GitSpecifier {
+    fn find_in(self, repo: &git2::Repository) -> Result<bool, Error> {
+        match self {
+            GitSpecifier::Hash(oid) => if let Err(e) = repo.find_commit(oid) {
+                match e.code() {
+                    git2::ErrorCode::NotFound => Ok(false),
+                    _ => bail!(e),
+                }
+            } else {
+                Ok(true)
+            },
+        }
+    }
+}
+
+pub fn check_cloned<S: AsRef<str>>(url: S) -> Result<LocalRepo, Error> {
     let mut url = Url::parse(url.as_ref())?;
-    {
+    let specifier = {
         let (mode, value) = {
             let mut args = url.query_pairs();
             ensure!(1 == args.count(), "there must be only one arg");
@@ -14,22 +40,41 @@ pub fn check_cloned<S: AsRef<str>>(url: S) -> Result<PathBuf, Error> {
         };
 
         match mode.as_ref() {
-            "rev" => (),
+            "rev" => GitSpecifier::Hash(value.parse()?),
             other => bail!("unsupported mode: {}", other),
         }
-    }
+    };
 
     url.set_query(None);
 
-    println!("{}", fs_safe_url(&url));
+    let mut path = PathBuf::from(".cache");
+    path.push(fs_safe_url(&url));
 
-    unimplemented!()
+    let repo = if !path.is_dir() {
+        fs::create_dir_all(&path)?;
+        git2::Repository::init_bare(&path)?
+    } else {
+        git2::Repository::open_bare(&path)?
+    };
+
+    if !specifier.find_in(&repo)? {
+        // empty fetch array here undocumented, but seems to work?
+        repo.remote("upstream", url.as_str())?
+            .fetch(&[], None, None)?;
+
+        ensure!(
+            specifier.find_in(&repo)?,
+            "fetching didn't help {:?} appear",
+            specifier
+        );
+    }
+
+    Ok(LocalRepo { path, specifier })
 }
 
 fn fs_safe_url(url: &Url) -> String {
     assert_eq!(None, url.query());
-    url.as_str()
-        .replace(|c: char| !c.is_alphanumeric(), "_")
+    url.as_str().replace(|c: char| !c.is_alphanumeric(), "_")
 }
 
 #[cfg(test)]
