7c010e7b10115f1ab70c45cf599bea05b08d432a
Chris West (Faux) <git@goeswhere.com>
2019-01-05 23:07:00 +0000

    set up parallel fetching, but don't use it, as the parallelism is infinite (?!)

--- src/fetch_images.rs
+++ src/fetch_images.rs
@@ -1,13 +1,14 @@
 use std::fs;
 
 use async_fetcher::AsyncFetcher;
+use failure::err_msg;
 use failure::Error;
+use futures::future::Future;
 use reqwest::r#async::Client;
 use tokio;
 
 pub fn fetch() -> Result<(), Error> {
     let client = Client::new();
-    let mut runtime = tokio::runtime::Runtime::new()?;
 
     let distros = &["trusty", "xenial", "bionic", "cosmic", "disco"];
 
@@ -15,18 +16,49 @@ pub fn fetch() -> Result<(), Error> {
 
     for distro in distros {
         fs::create_dir_all(distro)?;
-        let root = format!("https://partner-images.canonical.com/core/{}/current", distro);
+        let root = format!(
+            "https://partner-images.canonical.com/core/{}/current",
+            distro
+        );
 
-        for name in &["SHA256SUMS.gpg", "SHA256SUMS"] {
-            futures.push(AsyncFetcher::new(&client, format!("{}/{}", root, name))
-                .request_to_path(format!("{}/{}", distro, name).into())
-                .then_download(format!("{}/.{}.partial", distro, name).into())
-                .then_rename()
-                .into_future());
+        for (name, dest) in &[
+            ("SHA256SUMS.gpg".to_string(), "SHA256SUMS.gpg".to_string()),
+            ("SHA256SUMS".to_string(), "SHA256SUMS".to_string()),
+            (
+                format!("ubuntu-{}-core-cloudimg-amd64-root.tar.gz", distro),
+                "amd64-root.tar.gz".to_string(),
+            ),
+        ] {
+            let log_prefix = format!("{}: {}", distro, name);
+            futures.push(
+                AsyncFetcher::new(&client, format!("{}/{}", root, name))
+                    .with_progress_callback(move |ev| {
+                        use async_fetcher::FetchEvent::*;
+                        match ev {
+                            Get => println!("{}: Downloading...", log_prefix),
+                            DownloadComplete => println!("{}: Complete.", log_prefix),
+                            _ => (),
+                        }
+                    })
+                    .request_to_path(format!("{}/{}", distro, name).into())
+                    .then_download(format!("{}/.{}.partial", distro, name).into())
+                    .then_rename()
+                    .into_future(),
+            );
         }
     }
 
-    runtime.block_on(futures::future::join_all(futures))?;
+    let mut runtime = tokio::runtime::Runtime::new()?;
+    for future in futures {
+        runtime.block_on(future)?;
+    }
+
+    //runtime.block_on(futures::future::join_all(futures))?;
+
+    runtime
+        .shutdown_now()
+        .wait()
+        .map_err(|()| err_msg("failed during shutdown"))?;
 
     Ok(())
 }
