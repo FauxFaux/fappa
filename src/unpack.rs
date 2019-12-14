cc260bc058a5c19cad85146745d9c097f27cb925
Chris West (Faux) <git@goeswhere.com>
2019-12-14 16:20:35 +0000

    chore: extract as_ref

--- src/unpack.rs
+++ src/unpack.rs
@@ -6,10 +6,11 @@ use failure::ensure;
 use failure::Error;
 
 pub fn unpack<S: AsRef<Path>, D: AsRef<Path>>(src: S, dest: D) -> Result<(), Error> {
+    let dest = dest.as_ref();
     let file = fs::File::open(src)?;
     let unzipped = zstd::Decoder::new(file)?;
-    tar::Archive::new(unzipped).unpack(&dest)?;
-    let mut bin = dest.as_ref().to_path_buf();
+    tar::Archive::new(unzipped).unpack(dest)?;
+    let mut bin = dest.to_path_buf();
     bin.push("bin");
     ensure!(bin.metadata()?.is_dir(), "unpacked image contains a /bin");
     Ok(())
